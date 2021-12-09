3e5058782b79ac1f4305b7cf303d775cb553207c
Ubuntu <ubuntu@ip-172-31-25-198.ec2.internal>
2021-12-09 01:51:57 +0000

    First commit

---
+++ largest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Enter first  Number num1"
+read num1
+echo "Enter second Number num2"
+read num2
+echo "Enter the third number num3"
+read num3
+if  [ $num1 -gt $num2 ]; [ $num1 -gt $num2 ]
+then
+	echo "Num1 is greater $num1"
+elif [ $num2 -gt $num1 ]; [ $num2 -gt $num3 ]
+then
+       echo "Num2 is greater $num2"
+elif [ $num3 -gt $num1 ];[ $num3 -gt $num1 ]
+then
+  echo "Num3 is greater $num3"
+fi  
+
---
+++ name.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Name Of Employee"
+read name
+echo "Desgination"
+read desgination
+echo "Age Of Employee"
+read age
+echo "Company Name"
+read company
+echo "Name Of Employee $name,Desgination $desgination,Age Of Employee $age,Compnay Name $company"
+
